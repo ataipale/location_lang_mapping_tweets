2967dc494577f7fa87536997c55c90666b89177a
Alex Taipale <alextaipale@gmail.com>
2015-05-19 14:21:59 -0400

    Adding git ignore for large tweet storage, update get tweet

--- get_tweet.py
+++ get_tweet.py
@@ -5,21 +5,26 @@ import json
 import pickle
 
 #Set Twitter authentication keys
-ckey = os.environ.get('CKEY')
-ctoken = os.environ.get('CTOKEN')
-atoken = os.environ.get('ATOKEN')
-asecret = os.environ.get('ASECRET')
+CONSUMER_KEY    = os.environ.get('CONSUMER_KEY')
+CONSUMER_SECRET = os.environ.get('CONSUMER_SECRET')
+ACCESS_KEY      = os.environ.get('ACCESS_KEY')
+ACCESS_SECRET   = os.environ.get('ACCESS_SECRET')
     
 class CustomStreamListener(tweepy.streaming.StreamListener):
 
     def on_data(self, raw_data):
         tweet = {}
         line = json.loads(raw_data)
-        if line.get("coordinates") and line.get("lang"):
+        if line.get("coordinates") and line.get("lang") and line.get("text"):
+            tweet['id'] = line.get("id")
+            tweet['text'] = line.get("text")
             tweet['coordinates'] = line.get("coordinates")
             tweet['lang'] = line.get("lang")
-            with open('geolang.txt','a') as f:
+            if line.get("entities") and line.get("entities").get("hashtags"):
+                tweet['hashtags'] = line.get("entities").get("hashtags")
+            with open('id_txt_co_lang_hash.txt','a') as f:
                 pickle.dump(tweet, f)
+
         return True
 
     def on_error(self, status_code):
@@ -33,9 +38,9 @@ class CustomStreamListener(tweepy.streaming.StreamListener):
 if __name__ == '__main__':
 
     my_listener = CustomStreamListener()
-    auth = tweepy.OAuthHandler(ckey, ctoken)
+    auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 
-    auth.set_access_token(atoken, asecret)
+    auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
     sapi = tweepy.streaming.Stream(auth, my_listener)  
 
     # locations is given as East, North GPS of lower left coordinate and East, North of upper right GPS
